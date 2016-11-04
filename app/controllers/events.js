3a7b3a66c247d4712abfc1e818801aefc2aa55cf
PythoudGuillaume <guillaume.pythoud@unifr.ch>
2016-11-04 16:04:48 +0100

    POST and GET/ID endpoint

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -1,8 +1,9 @@
 'use strict';
-var Loadash = require('lodash');
+const Lodash = require('lodash');
 
 const Joi = require('joi');
-var Event = require('../database.js').User;
+var Event = require('../database.js').Event;
+const JS = require('joi-sequelize');
 
 
 module.exports.register = (server, options, next) => {
@@ -16,8 +17,8 @@ module.exports.register = (server, options, next) => {
     		Event.findAll({
 
     		}).then(function(events){
-    			if (req.query.limit === null) reply(events).code(200);
-    			else reply(Loadash.take(events, req.query.limit)).code(200)
+    			if (req.query.limit === undefined) reply(events).code(200);
+    			else reply(Lodash.take(events, req.query.limit)).code(200)
     			
     		})
     	},
@@ -36,13 +37,92 @@ module.exports.register = (server, options, next) => {
 	          		'responses': {
 		            	200: {
 		              		description: 'Success',
-		              		schema: Joi.array().items(Joi.string())
+		              		schema: Joi.array().items(Joi.string() //to do joi sequelize)
 		            	}
 	         		}
 	        	}
 	      	}
 	      }
 	});
+
+    server.route({
+        method: 'GET',
+        path: '/events/{id}',
+        handler: (req, reply) => {
+            req.query.limit
+            Event.findOne({
+                where:{
+                    id: req.params.id
+                }
+            }).then(function(event){
+                reply(event).code(200);
+                
+            })
+        },
+
+        config: {
+            tags: ['api'],
+            description: 'list all events/bets',
+            validate: {
+                params: {
+                    id: Joi.number().integer().required()
+                }
+            },
+            plugins: {
+                'hapi-swagger': {
+                    'responses': {
+                        200: {
+                            description: 'Success',
+                            schema: Joi.array().items(Joi.string() //tod joi sequelize)
+                        }
+                    }
+                }
+            }
+          }
+    });
+
+
+    server.route({
+        method: 'POST',
+        path: '/events',
+        handler: (req, reply) => {
+          let new_event = {
+          amount: req.payload.amount,
+          choice: req.payload.choice,
+          UserId: req.payload.userId
+          };
+          if(req.payload.bet == 'battle'){
+            new_event.battle = req.payload.betId
+          } else if ( req.payload.bet == 'bet'){
+            new_event.EventId = req.payload.betId
+          } else reply().code(400);
+          Event.create(new_event).then(function(new_event){
+              reply(new_event['id']).code(201);
+            }).catch(function(error){
+                reply('event not created').code(404);
+            })
+        },
+        config: {
+            tags: ['api'],  
+            description: 'add a new event',
+            validate: {
+                payload: {
+                    userId: Joi.number().integer().required(),
+                    bet: Joi.string().required(),
+                    betId: Joi.number().integer().required(),
+                    amount: Joi.number().required(),
+                    choice: Joi.boolean().required(),
+
+                }
+            },
+            plugins: {'hapi-swagger': {responses: {
+                201: { description: 'new event Created'},
+                400: {description: 'bad type'},
+                404: {description: 'id not found'}
+            }}}
+            }
+
+  });
 
 	return next();
 
