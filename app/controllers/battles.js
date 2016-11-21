a0eeb379913cbd422cf8b44762cb5d219343a0b8
Ismaïl Senhaji <ismail.senhaji@gmail.com>
2016-11-21 12:43:45 +0100

    Update /battle routes with new Battle class

--- app/controllers/battles.js
+++ app/controllers/battles.js
@@ -1,50 +1,108 @@
+'use strict';
 const _ = require('lodash');
-
 const request = require('request-json');
-const client = request.createClient('http://pokemon-battle.bid/api/v1/');
 const Joi = require('joi');
 
 module.exports.register = (server, options, next) => {
-  // TODO: Extract API URL to global variable
-  const client = request.createClient('http://pokemon-battle.bid/api/v1/');
+  const {Bet, Battle} = server.app.db;
+  const J = server.app.joi;
 
   // Routes covered in this module:
   // - /battles
   //  + GET: list of battles (default: battles that can still be bet on)
   //
   // - /battles/{id}
-  //  TODO+ GET: info about given battle
+  //  + GET: info about given battle
   //
   // - /battles/{id}/bets
-  //  TODO+ GET: list of bets placed on this battle
+  //  + GET: list of bets placed on this battle
   //  TODO+ POST: place a bet on this battle
 
   server.route({
     method: 'GET',
     path: '/battles',
     handler: (req, reply) => {
-      let response = [];
-      // TODO: add options to retrieve past battles, etc.
-      // TODO: transform battles (remove superfluous info, e.g. location)
       // TODO: get all battles with limit/offset
-      client.get(`battles/?limit=100&is_finished=false`).then((battles) => {
-        const now = new Date();
-        const response = battles.body
-          .filter((battle) => Date.parse(battle.start_time) < now)
-
-        reply(response).code(200);
-      })
+      Battle.getAll(req.query)
+        .then((battles) => reply(battles).code(200));
     },
 
     config: {
       tags: ['api'],
-      description: 'list upcoming battles',
+      description: 'List upcoming or past battles',
+      validate: {
+        query: {
+          isStarted: Joi.boolean().default(false),
+          isFinished: Joi.boolean().default(false),
+        }
+      },
       plugins: {
         'hapi-swagger': {
           'responses': {
             200: {
               description: 'Success',
               schema: Joi.array().items(Joi.string()) // TODO: replicate schema from remote API
+            }
+          }
+        }
+      }
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/battles/{id}',
+    handler: (req, reply) => {
+      Battle.get(req.params.id)
+        .then((battle) => reply(battles).code(200))
+        .catch((err) => reply(err).code(err.code));
+    },
+
+    config: {
+      tags: ['api'],
+      description: 'Get a battle',
+      validate: {
+        params: {
+          id: Joi.number().integer().positive().required()
+        }
+      },
+      plugins: {
+        'hapi-swagger': {
+          'responses': {
+            200: {
+              description: 'Success',
+              schema: Joi.array() // TODO: replicate schema from remote API
+            }
+          }
+        }
+      }
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/battles/{id}/bets',
+    handler: (req, reply) => {
+      Battle.get(req.params.id)
+        .then((battle) => battle.getBets())
+        .then((bets) => reply(bets).code(200))
+        .catch((err) => reply(err).code(err.code));
+    },
+
+    config: {
+      tags: ['api'],
+      description: 'List bets on a battle',
+      validate: {
+        params: {
+          id: Joi.number().integer().positive().required()
+        }
+      },
+      plugins: {
+        'hapi-swagger': {
+          'responses': {
+            200: {
+              description: 'Success',
+              schema: Joi.array().items(J.Bet.joi()) // TODO: add relations
             }
           }
         }
