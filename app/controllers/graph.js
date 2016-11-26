4140b7249dd075ac5df34f0136509512445e3b1c
Ismaïl Senhaji <ismail.senhaji@gmail.com>
2016-11-26 14:40:39 +0100

    Nicer graph

--- app/controllers/graph.js
+++ app/controllers/graph.js
@@ -3,24 +3,64 @@ const Joi = require('joi')
 
 module.exports.register = (server, options, next) => {
   // TODO: Extract API URL to global variable
-  const Bet = server.app.db.models.Bet
+  const { Battle, Bet } = server.app.db.models
 
-  const edge = (bet) => (bet.ParentId === null ? `"B${bet.BattleId}"` : `"E${bet.ParentId}"`) + ` -> "E${bet.id}";`
+  function graph (bets) {
+    let graph = bets.map(node).concat(bets.map(edge))
+    let ids = bets.map((b) => b.BattleId)
+    return Battle
+      .findAll({ where: { id: { $in: [...new Set(ids)] } } })
+      .then((battles) => battles.map(node).concat(graph))
+      .then((graph) => graph.join('\n  '))
+      .then((graph) => `digraph {\n  ${graph}\n  \n}\n`)
+  }
+
+  function node (event) {
+    if (event.Model === Battle) {
+      const node = `battles/${event.id}`
+      const label = `${event.id}\nresult:${event.result}`
+      const color = event.result === null ? 'white' : event.result === 1 ? 'cyan' : 'yellow'
+      return `"${node}"[shape=invhouse,label="${label}",style=filled,fillcolor=${color}]`
+    } else {
+      const node = `bets/${event.id}`
+      const label = `${event.id}\nusers/${event.UserId}\nchoice:${event.choice}\nresult:${event.result}`
+      const color = event.won === null ? 'white' : event.won ? 'green' : 'red'
+      return `"${node}"[shape=box,label="${label}",style=filled,fillcolor=${color}]`
+    }
+  }
+
+  function edge (bet) {
+    let A =
+      bet.ParentId === null
+      ? `"battles/${bet.BattleId}"`
+      : `"bets/${bet.ParentId}"`
+
+    let B = `"bets/${bet.id}"`
+
+    return `${A} -> ${B};`
+  }
 
   server.route({
     method: 'GET',
     path: '/graph',
     handler: (req, reply) => {
-      Bet.findAll()
-        .then((bets) => bets.map(edge))
-        .then((edges) => edges.join('\n  '))
-        .then((edges) => `digraph {\n${edges}\n  node [shape = circle]\n}\n`)
+      Bet
+        .scope(req.query.status)
+        .findAll()
+        .then(graph)
         .then((graph) => reply(graph).code(200))
     },
 
     config: {
       tags: ['api'],
       description: 'Graph of bets in dot format',
+      validate: {
+        query: {
+          status: Joi.string()
+            .valid(['active', 'started', 'finished'])
+        }
+      },
+
       plugins: {
         'hapi-swagger': {
           'responses': {
