e65e6d53a58bb897b75181226ca84190c3971858
Ismaïl Senhaji <ismail.senhaji@gmail.com>
2016-12-06 19:29:36 +0100

    Add Pokémon images

--- app/controllers/pokemons.js
+++ app/controllers/pokemons.js
@@ -1,5 +1,11 @@
 'use strict'
 const Joi = require('joi')
+const fs = require('fs')
+
+const exists = (path) => new Promise((resolve, reject) => {
+  fs.stat(path, (err, stat) => !err ? resolve(true)
+      : err.code === 'ENOENT' ? resolve(false) : reject(err))
+})
 
 module.exports.register = (server, options, next) => {
   const { Pokemon } = server.app.db.models
@@ -64,6 +70,54 @@ module.exports.register = (server, options, next) => {
       validate: {
         params: {
           id: J.ID.required()
+        }
+      },
+
+      plugins: {
+        'hapi-swagger': {
+          'responses': {
+            200: {
+              description: 'Success',
+              schema: J.Pokemon.joi()
+            },
+            404: {
+              description: 'Pokemon not found',
+              schema: Joi.object()
+            }
+          }
+        }
+      }
+    }
+  })
+
+  // GET /pokemons/{id}/image
+  server.route({
+    method: 'GET',
+    path: '/pokemons/{id}/image',
+    handler: (req, reply) => {
+      const image = `${req.params.id}.png`
+      const paths = {
+        small: `./node_modules/pokemon-sprites/sprites/pokemon/${image}`,
+        large: `./node_modules/pokemon-sprites/sprites/pokemon/model/${image}`
+      }
+
+      exists(paths[req.query.size])
+        .then((exists) => exists ? paths[req.query.size] : paths.small)
+        .then((path) => reply.file(path))
+        .catch(reply)
+    },
+
+    config: {
+      tags: ['api'],
+      description: 'Get a pokemon image',
+      validate: {
+        params: {
+          id: J.ID.required()
+        },
+        query: {
+          size: Joi.string()
+            .valid(['small', 'large'])
+            .default('small')
         }
       },
 
